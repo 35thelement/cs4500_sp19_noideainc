139c27e93c8af5cd027796d0e9d5adda7f37977b
bowenleis <lei.b@husky.neu.edu>
2019-02-08 13:02:55 -0500

    create FrequentlyAskedAnswerRepository

---
+++ src/main/java/com/example/cs4500_sp19_noideainc/repositories/FrequentlyAskedAnswerRepository.java
@@ -0,0 +1,24 @@
+package com.example.cs4500_sp19_noideainc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.cs4500_sp19_noideainc.models.FrequentlyAskedAnswer;
+
+//This is the repository for FrequentlyAskedAnswer class, which 
+//will implement all database operations related the FrequentlyAskedAnswer Data Model
+public interface FrequentlyAskedAnswerRepository extends CrudRepository<FrequentlyAskedAnswer, Integer> {
+
+	// find all answers for FAQs in the table
+	@Query(value="SELECT frequentlyAskedAnswer FROM FrequentlyAskedAnswer frequentlyAskedAnswer")
+	public List<FrequentlyAskedAnswer> findAllFrequentlyAskedAnswers();
+	
+	// find the specific answer by given ID
+	@Query(value="SELECT frequentlyAskedAnswer FROM FrequentlyAskedAnswer frequentlyAskedAnswer WHERE frequentlyAskedAnswer.id=:id")
+	public FrequentlyAskedAnswer findFrequentlyAskedAnswerById(@Param("id") Integer id);
+	
+	
+}
