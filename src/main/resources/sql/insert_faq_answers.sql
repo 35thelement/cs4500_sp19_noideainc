ce25305453d8b01f854c2eb6b472b129a23edee0
Duoduo <duoduo@Duoduos-MacBook-Pro.local>
2019-02-08 22:16:51 -0500

    updated faq answers to make more sense

--- src/main/resources/sql/insert_faq_answers.sql
+++ src/main/resources/sql/insert_faq_answers.sql
@@ -1,5 +1,5 @@
-INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (1, 'No', 1, 1);
-INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (2, 'No', 1, 2);
-INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (3, 'Yes', 2, 1);
-INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (4, 'No', 2, 2);
-INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (5, 'No', 1, 3);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (1, 'Yes', 1, 123);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (2, 'Yes', 1, 234);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (3, '500', 2, 123);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (4, '600', 2, 234);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (5, '3 years', 3, 345);
