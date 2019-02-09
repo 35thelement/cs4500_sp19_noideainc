c5be7236e39ac2b8c8c841cbcb70f17dbbad0d01
Duoduo <duoduo@Duoduos-MacBook-Pro.local>
2019-02-08 21:41:33 -0500

    added insert_faq_answers sql file

---
+++ src/main/resources/sql/insert_faq_answers.sql
@@ -0,0 +1,5 @@
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (1, 'No', 1, 1);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (2, 'No', 1, 2);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (3, 'Yes', 2, 1);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (4, 'No', 2, 2);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (5, 'No', 1, 3);
