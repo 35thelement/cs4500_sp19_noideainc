b5a2a74c2c15221bf6106d40457a6190e18c3de3
skamlangjai <meieing@gmail.com>
2019-02-08 19:32:09 -0500

    add delete faq and find all answers by faqid methods

--- src/main/java/com/example/cs4500_sp19_noideainc/services/FrequentlyAskedQuestionService.java
+++ src/main/java/com/example/cs4500_sp19_noideainc/services/FrequentlyAskedQuestionService.java
@@ -3,12 +3,14 @@ package com.example.cs4500_sp19_noideainc.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.cs4500_sp19_noideainc.models.FrequentlyAskedAnswer;
 import com.example.cs4500_sp19_noideainc.models.FrequentlyAskedQuestion;
 import com.example.cs4500_sp19_noideainc.repositories.FrequentlyAskedQuestionRepository;
 
@@ -35,5 +37,15 @@ public class FrequentlyAskedQuestionService {
 		FrequentlyAskedQuestion faq = faqRepository.findFrequentlyAskedQuestionById(id);
 		faq.setQuestion(faqUpdates.getQuestion());
 		return faqRepository.save(faq);
+	}
+	@DeleteMapping("/api/faqs/{faqId}")
+	public void deleteFrequentlyAskedQuestion(
+			@PathVariable("faqId") Integer id) {
+		faqRepository.deleteById(id);
+	}
+	@GetMapping("/api/faqs/{faqId}/answers")
+	public List<FrequentlyAskedAnswer> findAllFrequentlyAskedAnswersByFAQId(
+			@PathVariable("faqId") Integer id) {
+		return faqRepository.findAllFrequentlyAskedAnswersByFAQId(id);
 	}
 }
