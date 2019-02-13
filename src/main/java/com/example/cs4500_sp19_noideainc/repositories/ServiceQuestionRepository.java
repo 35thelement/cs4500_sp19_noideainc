99c9e96829881e1c4434e794901952f2101585b4
alexjordan98 <alexjordan98@gmail.com>
2019-02-13 18:15:14 -0500

    Added Service Question Interface, restful and model

---
+++ src/main/java/com/example/cs4500_sp19_noideainc/repositories/ServiceQuestionRepository.java
@@ -0,0 +1,18 @@
+package com.example.cs4500_sp19_noideainc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.cs4500_sp19_noideainc.models.ServiceQuestion;
+
+public interface ServiceQuestionRepository
+        extends CrudRepository<ServiceQuestion, Integer> {
+  @Query(value="SELECT entity FROM ServiceQuestion entity")
+  public List<ServiceQuestion> findAllServiceQuestions();
+  @Query(value="SELECT entity FROM ServiceQuestion entity WHERE id=:id")
+  public ServiceQuestion findServiceQuestionById(
+          @Param("id") Integer id);
+}
