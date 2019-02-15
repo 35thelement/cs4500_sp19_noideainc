252f20f5c0af0231283a2d8d61fc2ed2e96f5c2b
Brian Hughes <brianhughes@Brians-MacBook-Pro.local>
2019-02-14 19:14:53 -0500

    updates service question and answer sql tables and inserts

--- src/main/resources/sql/create_service_specific_question.sql
+++ src/main/resources/sql/create_service_specific_question.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS `service_questions`;
 CREATE TABLE `service_questions` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  true_false_answer varchar(255) DEFAULT NULL,
-  max_range varchar(255) DEFAULT NULL,
-  min_range varchar(255) DEFAULT NULL,
-  choice varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar (255) DEFAULT NULL,
+  `question` varchar(255) DEFAULT NULL,
+  `question_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/main/resources/sql/insert_service_question.sql
@@ -0,0 +1,4 @@
+INSERT INTO service_questions (id, title, question, question_type, choice) VALUES (1, 'How Many Rooms?', 'How many rooms do you need to be cleaned', 'choice');
+INSERT INTO service_questions (id, title, question, question_type, choice) VALUES (1, 'pets', 'do you have any pets?', 'true or false');
+INSERT INTO service_questions (id, title, question, question_type, choice) VALUES (1, 'timing', 'what times are you available for today', 'min max');
+
