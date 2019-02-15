11d07cf39f9aad24769fe0fa12b7cb4809aa64b1
Ben Romine <elementnumber35@gmail.com>
2019-02-14 22:01:40 -0500

    Revert "removed the bin folder"

---
+++ bin/src/main/resources/sql/create_service_answers.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `service_answers`;
+CREATE TABLE `service_answers` (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  true_false_answer varchar(255) DEFAULT NULL,
+  max_range varchar(255) DEFAULT NULL,
+  min_range varchar(255) DEFAULT NULL,
+  choice varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ bin/src/main/resources/sql/insert_faq_answers.sql
@@ -0,0 +1,5 @@
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (1, 'Yes', 1, 123);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (2, 'Yes', 1, 234);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (3, '500', 2, 123);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (4, '600', 2, 234);
+INSERT INTO frequently_asked_answers (id, answer, frequently_asked_question_id, user_id) VALUES (5, '3 years', 3, 345);
