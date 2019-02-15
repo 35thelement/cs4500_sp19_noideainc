300b737398283f8f3daf0e2dde251b161f5d3edd
Juan Lopez <lopez.ju@husky.neu.edu>
2019-02-14 21:55:30 -0500

    removed the bin folder

--- bin/src/main/resources/sql/insert_frequently_asked_questions.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO frequently_asked_questions (question, title) VALUES ('Have you passed a background check?', 'background check');
-INSERT INTO frequently_asked_questions (question, title) VALUES ('How many employees does your company have?', 'number of employees');
-INSERT INTO frequently_asked_questions (question, title) VALUES ('How long have your company been operating for?', 'company\'s age');
-INSERT INTO frequently_asked_questions (question, title) VALUES ('Who is the founder of your company?', 'company founder');
-INSERT INTO frequently_asked_questions (question, title) VALUES ('Are there any promotional offers?', 'promotional offers');
-INSERT INTO frequently_asked_questions (question, title) VALUES ('What sets you apart from other service providers?', 'selling point');
