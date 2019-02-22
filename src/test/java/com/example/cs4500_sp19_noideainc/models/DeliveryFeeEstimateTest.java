34ddb1a99a0c65910e85bc8e2d60d69d66292672
bowenleis <lei.b@husky.neu.edu>
2019-02-22 14:17:31 -0500

    make getFees function to cover error situations

--- src/test/java/com/example/cs4500_sp19_noideainc/models/DeliveryFeeEstimateTest.java
+++ src/test/java/com/example/cs4500_sp19_noideainc/models/DeliveryFeeEstimateTest.java
@@ -10,37 +10,64 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DeliveryFeeEstimateTest {
-	// default service and user
+	// Default service and user
 	User user = new User();
 	Service service = new Service();
 	
-	// all current frequency and related deliverFee class (all of them are flat fees)
-	private final DeliveryFee deliveryFee1 = new DeliveryFee(0, Frequency.Weekday, true);
-	private final DeliveryFee deliveryFee2 = new DeliveryFee(75f, Frequency.Weekend, true);
-	private final DeliveryFee deliveryFee3 = new DeliveryFee(150f, Frequency.Holiday, true);
-	private final DeliveryFee deliveryFee4 = new DeliveryFee(225f, Frequency.Emergency, true);
+	// Default value of baseFrequency and subscriptionFrequency
+	Frequency baseFrequency = Frequency.Daily;
+	Frequency subscriptionFrequency = Frequency.Daily;
 	
-	private List<DeliveryFee> listDeliveryFee1 = new ArrayList<DeliveryFee>();
+	// All basic frequency and related deliverFee class (all of them are flat fees)
+	private final DeliveryFee weekdayFlat = new DeliveryFee(0, Frequency.Weekday, true);
+	private final DeliveryFee weekendFlat = new DeliveryFee(75f, Frequency.Weekend, true);
+	private final DeliveryFee holidayFlat = new DeliveryFee(150f, Frequency.Holiday, true);
+	private final DeliveryFee EmergencyFlat = new DeliveryFee(225f, Frequency.Emergency, true);
+	
+//	
+//	@Test
+//	// Test regular situations for the calculation of different flat fees
+//	public void testFlatFees() {
+//		// a list of deliveryFees
+//		List<DeliveryFee> listDeliveryFee = new ArrayList<DeliveryFee>();
+//		listDeliveryFee.add(this.weekdayFlat);
+//		listDeliveryFee.add(this.weekendFlat);
+//		listDeliveryFee.add(this.holidayFlat);
+//		listDeliveryFee.add(this.EmergencyFlat);
+//		
+//		Estimate estimate = new Estimate(0f, 750f, baseFrequency, false, subscriptionFrequency, 
+//				Frequency.Weekday, service, user);
+//		
+//		// Test no frequency are added on the base price
+//		assertEquals(0f, estimate.getFees(listDeliveryFee));
+//		
+//		// Test delivery frequency with different fat fee
+//		estimate.setDeliveryFrequency(Frequency.Weekend);
+//		assertEquals(75f, estimate.getFees(listDeliveryFee));
+//		
+//		estimate.setDeliveryFrequency(Frequency.Holiday);
+//		assertEquals(150f, estimate.getFees(listDeliveryFee));
+//		
+//		estimate.setDeliveryFrequency(Frequency.Emergency);
+//		assertEquals(225f, estimate.getFees(listDeliveryFee));
+//		
+//	}
+//	
+//	@Test
+//	// Test special situations for the calculation of different flat fees
+//	public void testFlatFees1() {
+//		// a list of deliveryFees
+//		List<DeliveryFee> listDeliveryFee = new ArrayList<DeliveryFee>();
+//		DeliveryFee weekdayFlat1 = new DeliveryFee(15, Frequency.Weekday, true);
+//		listDeliveryFee.add(weekdayFlat1);
+//		Estimate estimate = new Estimate(0f, 500f, baseFrequency, false, subscriptionFrequency, 
+//				Frequency.Weekday, service, user);
+//		assertEquals(true, estimate.getFees(listDeliveryFee) > 0);
+//	}
 	
 	
-	@Test
-	// test if the calculation of different flat fees are correct
-	public void testFlatFees() {
-		this.listDeliveryFee1.add(this.deliveryFee1);
-		this.listDeliveryFee1.add(this.deliveryFee2);
-		this.listDeliveryFee1.add(this.deliveryFee3);
-		this.listDeliveryFee1.add(this.deliveryFee4);
-		Estimate estimate = new Estimate(0f, 750f, Frequency.Weekday, false, Frequency.Weekday, Frequency.Weekday, service, user);
-		// test no frequency are added on the base price
-		assertEquals(0f, estimate.getFees(this.listDeliveryFee1));
-		
-		// test delivery frequency with fat fee
-		estimate.setDeliveryFrequency(Frequency.Weekend);
-		assertEquals(75f, estimate.getFees(this.listDeliveryFee1));
-		estimate.setDeliveryFrequency(Frequency.Holiday);
-		assertEquals(150f, estimate.getFees(this.listDeliveryFee1));
-		estimate.setDeliveryFrequency(Frequency.Emergency);
-		assertEquals(225f, estimate.getFees(this.listDeliveryFee1));
-		
-	}
 }
+
+
+
+
