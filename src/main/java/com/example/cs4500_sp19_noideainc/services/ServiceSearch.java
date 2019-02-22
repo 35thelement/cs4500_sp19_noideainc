39656b698a4ac19e6ffae6c21a5e2eb452b3f137
alexjordan98 <alexjordan98@gmail.com>
2019-02-21 20:44:58 -0500

    added serviceSearch

---
+++ src/main/java/com/example/cs4500_sp19_noideainc/services/ServiceSearch.java
@@ -0,0 +1,22 @@
+package com.example.cs4500_sp19_noideainc.services;
+
+import com.example.cs4500_sp19_noideainc.models.Service;
+import com.example.cs4500_sp19_noideainc.models.User;
+
+import java.util.List;
+
+public class ServiceSearch {
+
+
+
+  List<User> searchForProviders(Service service, SearchCriteria criteria) {
+
+
+
+
+    return null;
+  }
+
+
+
+}
