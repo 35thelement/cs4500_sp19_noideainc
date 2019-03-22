8d83fff2274b3a13f89bdd514ee16a71986d24ca
Ben Romine <elementnumber35@gmail.com>
2019-03-22 14:57:50 -0400

    More User JUnit Tests

--- src/test/java/com/example/cs4500_sp19_noideainc/services/UserServiceTest.java
+++ src/test/java/com/example/cs4500_sp19_noideainc/services/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.cs4500_sp19_noideainc.services;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -11,33 +12,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 
 import com.example.cs4500_sp19_noideainc.models.*;
+import com.example.cs4500_sp19_noideainc.repositories.ServiceRepository;
+import com.example.cs4500_sp19_noideainc.repositories.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserService.class)
+@AutoConfigureMockMvc
 public class UserServiceTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private UserService userService;
+    private UserRepository userRepository;
+    @MockBean
+    private ServiceRepository serviceRepository;
 
     private User nate = new User(123, "nate", "password", "Nate", "Jones");
     private User sam = new User(234, "sam", "password", "Sam", "Smith");
+    private Service service = new Service(1, "landscaping", "making your yard look fancy");
 
     @Test
     public void testFindUserById() throws Exception {
-        when(userService.findUserById(123)).thenReturn(nate);
+        when(userRepository.findUserById(123)).thenReturn(nate);
         this.mockMvc
                 .perform(get("/api/users/123"))
                 .andExpect(status().isOk())
@@ -50,7 +57,7 @@ public class UserServiceTest {
 
     @Test
     public void testFindAllUsers() throws Exception {
-        when(userService.findAllUser()).thenReturn(Arrays.asList(nate, sam));
+        when(userRepository.findAllUsers()).thenReturn(Arrays.asList(nate, sam));
         this.mockMvc
                 .perform(get("/api/users"))
                 .andExpect(status().isOk())
@@ -63,7 +70,7 @@ public class UserServiceTest {
         ObjectMapper nateMapper = new ObjectMapper();
 
 
-        when(userService.createUser(nate)).thenReturn(sam);
+        when(userRepository.save(nate)).thenReturn(sam);
         this.mockMvc
                 .perform(post("/api/users")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -76,13 +83,24 @@ public class UserServiceTest {
     public void testUpdateUser() throws Exception {
         ObjectMapper nateMapper = new ObjectMapper();
 
-        User theCoolerNate = new User(123, "cooler_nate", "password", "Nate", "Jones");
+        User theCoolerNate = new User(123, "cooler_nate", "passwd", "Nathan", "Johnson");
 
-        when(userService.updateUser(nate.getId(), theCoolerNate)).thenReturn(nate);
+        when(userRepository.save(nate)).thenReturn(theCoolerNate);
+        when(userRepository.findUserById(123)).thenReturn(theCoolerNate);
         this.mockMvc
                 .perform(put("/api/users/123")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content(nateMapper.writeValueAsString(theCoolerNate)))
+                        .content(nateMapper.writeValueAsString(nate)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+        doNothing().when(userRepository).deleteById(123);
+        this.mockMvc
+                .perform(delete("/api/users/123")
+                        .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
