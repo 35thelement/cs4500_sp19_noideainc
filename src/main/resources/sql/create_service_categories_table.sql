78d603562132f222fc1c983eeaa9ab852eb39c91
skamlangjai <kamlangjai.s@northeastern.edu>
2019-04-05 13:53:40 -0400

    add insert statements for services & categories with score

--- src/main/resources/sql/create_service_categories_table.sql
+++ src/main/resources/sql/create_service_categories_table.sql
@@ -2,5 +2,7 @@ CREATE TABLE `service_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `score` int(11) DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `service_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`service_id`) REFERENCES `services` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
