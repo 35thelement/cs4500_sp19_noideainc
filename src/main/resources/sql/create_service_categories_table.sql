af5051224101d8266e31e19e1ffd4ccced9dfbc3
skamlangjai <kamlangjai.s@northeastern.edu>
2019-04-05 14:02:04 -0400

    undo change

--- src/main/resources/sql/create_service_categories_table.sql
+++ src/main/resources/sql/create_service_categories_table.sql
@@ -2,7 +2,5 @@ CREATE TABLE `service_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `score` int(11) DEFAULT 0,
-  `service_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`service_id`) REFERENCES `services` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
