15a78b0a83bb8bcfd89f1d49af7f576ba380eac3
bowenleis <lei.b@husky.neu.edu>
2019-04-08 23:23:36 -0400

    update sql of create-user-table for login

--- src/main/resources/sql/create_users_tables.sql
+++ src/main/resources/sql/create_users_tables.sql
@@ -3,7 +3,8 @@ CREATE TABLE `users` (
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=346 DEFAULT CHARSET=utf8;
