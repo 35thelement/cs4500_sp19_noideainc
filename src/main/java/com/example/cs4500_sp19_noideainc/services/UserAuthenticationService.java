b76a4fadcc32197b31b5c682be8626bb6a2a32ea
Ben Romine <elementnumber35@gmail.com>
2019-04-10 14:02:56 -0400

    hotfix for UserAuthService

--- src/main/java/com/example/cs4500_sp19_noideainc/services/UserAuthenticationService.java
+++ src/main/java/com/example/cs4500_sp19_noideainc/services/UserAuthenticationService.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
