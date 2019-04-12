2b8ce5115b0253fedf9b9ae9c103cdbe74672354
alexjordan98 <alexjordan98@gmail.com>
2019-04-11 23:06:51 -0400

    Backend changs for mock data

--- src/main/java/com/example/cs4500_sp19_noideainc/models/AvailablePaymentMethods.java
+++ src/main/java/com/example/cs4500_sp19_noideainc/models/AvailablePaymentMethods.java
@@ -1,5 +1,8 @@
 package com.example.cs4500_sp19_noideainc.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AvailablePaymentMethods {
     Bitcoin,
     Cash,
